6765e7b990100d21ee114b8876d9e36469aed46a
dwiki <rinaldidwiki@gmail.com>
2019-11-02 21:41:29 +0700

    file kedua saya

---
+++ labspy02.py
@@ -0,0 +1,9 @@
+a =int(input("Masukan bilangan a = "))
+b =int(input("Masukan bilangan b = "))
+c =int(input("Masukan bilangan c = "))
+if a > b and a > c:
+    print (" Bilangan terbesar nya = ",a)
+elif b > a and b > c:
+    print (" Bilangan terbesar nya = ",b)
+else:
+    print (" Bilangan terbesar nya = ",c)
